23719423b6dcf05de8e06c67a3338fe32dd57488
Ruben <rgarciarios95@gmail.com>
2023-10-30 15:19:02 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,53 +1,51 @@
-// Create a web server for comments
-// Run it with: node comments.js
-// Then, open http://localhost:8000/comments.html in a browser to see the comments page.
+// Create simple web application that responds to requests to /comments and
+// /comments/new. The /comments/new path should accept a POST request and add
+// the submitted comment to the comment list. The /comments path should
+// display all of the submitted comments. Use the following HTML as a starting
+// point for your application:
 
 var http = require('http');
-var fs = require('fs');
-var url = require('url');
+var qs = require('querystring');
 
 var comments = [];
 
-// Create a server that will respond to requests for comments
-var server = http.createServer(function (request, response) {
-    // Get the path from the URL
-    var path = url.parse(request.url).pathname;
-    // If the path is /comments, then we are adding a comment
-    if (path == '/comments') {
-        // Get the comment from the URL query string
-        var comment = url.parse(request.url, true).query.comment;
-        // Add the comment to the list of comments
-        comments.push(comment);
-        // Send back a response
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.end('Thanks for your comment!');
-    }
-    // If the path is /show, then we are showing all comments
-    else if (path == '/show') {
-        // Send back a response
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.end(comments.join('\n'));
-    }
-    // Otherwise, we are requesting a web page
-    else {
-        // Get the file name from the path
-        var fileName = path.substring(1);
-        // Read the file asynchronously
-        fs.readFile(fileName, function(err, data) {
-            // If there is an error, send back a 404 page
-            if (err) {
-                response.writeHead(404, {'Content-Type': 'text/html'});
-                response.end('404 Not Found');
-            }
-            // Otherwise, send back the file
-            else {
-                response.writeHead(200, {'Content-Type': 'text/html'});
-                response.end(data.toString());
-            }
-        });
-    }
-});
+function renderForm(comment) {
+  var html = '<!doctype html>';
+  html += '<html><head><title>Guest Book</title></head>';
+  html += '<body><h1>Guest Book</h1>';
+  html += '<form method="post" action="/comments/new">';
+  html += '<input type="text" name="comment">';
+  html += '<input type="submit" value="Submit">';
+  html += '</form>';
+  html += '<h2>Comments</h2>';
+  html += '<ul>';
+  comments.forEach(function(comment) {
+    html += '<li>' + comment + '</li>';
+  });
+  html += '</ul>';
+  html += '</body></html>';
+  return html;
+}
 
-// Start listening for requests
-server.listen(8000);
-console.log('Server running at http://localhost:8000');
+http.createServer(function(req, res) {
+  if (req.method === 'POST' && req.url === '/comments/new') {
+    var body = '';
+    req.on('data', function(data) {
+      body += data;
+    });
+    req.on('end', function() {
+      var comment = qs.parse(body).comment;
+      comments.push(comment);
+      res.writeHead(302, { 'Location': '/comments' });
+      res.end();
+    });
+  } else if (req.url === '/comments') {
+    var html = renderForm();
+    res.setHeader('Content-Type', 'text/html');
+    res.statusCode = 200;
+    res.end(html);
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+}).listen(3000); server
